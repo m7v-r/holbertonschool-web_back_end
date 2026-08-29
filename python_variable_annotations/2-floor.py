098f617ea7730ca4b47c2c4b4889b5dba51964b0
m7v-r <ms4571690@icloud.com>
2026-08-29 19:44:58 +0300

    Task 2: Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor module.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float number as an int.
+    """
+    return math.floor(n)
