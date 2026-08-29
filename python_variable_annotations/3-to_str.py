913170d3a0a0601d0425048b45c173f86bb97ccc
m7v-r <ms4571690@icloud.com>
2026-08-29 19:49:58 +0300

    Task 3: Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string module.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float n as argument and returns its string representation.
+    """
+    return str(n)
