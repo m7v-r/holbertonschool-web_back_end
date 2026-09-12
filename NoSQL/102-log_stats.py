b76a36f41defeb4068b593916d7ffb3019211fc2
m7v-r <ms4571690@icloud.com>
2026-09-12 19:35:20 +0300

    15. Log stats - new version

---
+++ NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Provides stats about Nginx logs stored in MongoDB including top IPs """
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ Displays stats about Nginx logs """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(status_check))
+
+    print("IPs:")
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = nginx_collection.aggregate(pipeline)
+    for ip in top_ips:
+        print("\t{}: {}".format(ip.get('_id'), ip.get('count')))
+
+
+if __name__ == "__main__":
+    log_stats()
