db18dbebf9782e5472a4f7dfcefe2138e302ea3f
m7v-r <ms4571690@icloud.com>
2026-09-12 19:34:09 +0300

    14. Top students

---
+++ NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module that returns all students sorted by average score """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    pipeline = [
+        {
+            "$project": {
+                "name": "$name",
+                "topics": "$topics",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+    return list(mongo_collection.aggregate(pipeline))
