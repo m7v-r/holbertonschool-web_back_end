474c0d5ae786bef9be107d5e466da9c5e3a6bb37
m7v-r <ms4571690@icloud.com>
2026-09-19 17:07:47 +0300

    Fix 0-constants.js implementation

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,14 +1,15 @@
 export function taskFirst() {
-  const task = 'Prefer const when you can.';
+  const task = 'I prefer const when I can.';
   return task;
 }
 
 export function getLast() {
-  return 'is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += ` ${getLast()}`;
+  combination += getLast();
+
   return combination;
 }
