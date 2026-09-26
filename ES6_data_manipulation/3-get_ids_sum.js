675a0b8fb15e7382939c78b48030019d9f73b581
m7v-r <ms4571690@icloud.com>
2026-09-26 17:31:12 +0300

    Add Task 3 solution

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+}
